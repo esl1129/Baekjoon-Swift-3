6d9bdf9ea8ab1b4699462d1b1cd6ff584df1ae3c
Jaewook Lim <esl112999@gmail.com>
2022-02-05 15:10:58 +0900

    **Problem 18311 Update 2022.02.05**

---
+++ Pages/18311.xcplaygroundpage/Contents.swift
@@ -0,0 +1,92 @@
+import Foundation
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var now = read()
+        
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+        
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+        
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+
+func solution() -> Int {
+    let fIO = FileIO()
+    let N = fIO.readInt()
+    let K = fIO.readInt()
+    var answer = 1
+    var len = 0
+    
+    var course = [Int]()
+    for _ in 0..<N{
+        course.append(fIO.readInt())
+    }
+    
+    for c in course {
+        if K < len+c {
+            return answer
+        }
+        len += c
+        answer += 1
+    }
+    answer -= 1
+    for c in course.reversed() {
+        if K < len+c {
+            return answer
+        }
+        len += c
+        answer -= 1
+    }
+    
+    return answer
+}
+
+print(solution())
